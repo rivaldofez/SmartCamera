adbf8f7e2477ad1bf7bffef5d20f8a3553d92271
rivaldofez <rivaldofez@gmail.com>
2022-05-16 23:59:52 +0700

    Initial Commit

---
+++ SmartCamera/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  SmartCamera
+//
+//  Created by Rivaldo Fernandes on 16/05/22.
+//
+
+import UIKit
+import AVKit
+import Vision
+
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        //here to start the camera
+//        let captureSession = AVCaptureSession()
+//        guard let captureDevice = AVCaptureDevice.default(for: .video) else {
+//            return
+//        }
+//        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+//
+//        captureSession.addInput(input)
+//        captureSession.sessionPreset = .photo
+//
+//        captureSession.startRunning()
+//
+//        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+//        view.layer.addSublayer(previewLayer)
+//        previewLayer.frame = view.frame
+//
+//
+//        let dataOutput = AVCaptureVideoDataOutput()
+//        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoqueue"))
+//        captureSession.addOutput(dataOutput)
+        
+//        VNImageRequestHandler(cgImage: <#T##CGImage#>, options: [:]).perform(<#T##requests: [VNRequest]##[VNRequest]#>)
+    }
+    
+//    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+//        print("Camera was ")
+//    }
+
+
+}
+
