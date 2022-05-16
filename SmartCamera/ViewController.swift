defdfbbfd3dd311b2946af7ef0d69d89229ce1ed
rivaldofez <rivaldofez@gmail.com>
2022-05-17 00:00:40 +0700

    add camera reader

--- SmartCamera/ViewController.swift
+++ SmartCamera/ViewController.swift
@@ -16,25 +16,25 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         // Do any additional setup after loading the view.
         
         //here to start the camera
-//        let captureSession = AVCaptureSession()
-//        guard let captureDevice = AVCaptureDevice.default(for: .video) else {
-//            return
-//        }
-//        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
-//
-//        captureSession.addInput(input)
-//        captureSession.sessionPreset = .photo
-//
-//        captureSession.startRunning()
-//
-//        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-//        view.layer.addSublayer(previewLayer)
-//        previewLayer.frame = view.frame
-//
-//
-//        let dataOutput = AVCaptureVideoDataOutput()
-//        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoqueue"))
-//        captureSession.addOutput(dataOutput)
+        let captureSession = AVCaptureSession()
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {
+            return
+        }
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        
+        captureSession.addInput(input)
+        captureSession.sessionPreset = .photo
+        
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+        
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoqueue"))
+        captureSession.addOutput(dataOutput)
         
 //        VNImageRequestHandler(cgImage: <#T##CGImage#>, options: [:]).perform(<#T##requests: [VNRequest]##[VNRequest]#>)
     }
